74cbd538c8c46ef26ab930d22704f1d73a95ed50
evgenij <chucknorristhegreat@yandex.ru>
2024-11-28 21:21:49 +0300

    initial commit

---
+++ src/main/java/ru/krupnoveo/edu/record_service/RecordServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.krupnoveo.edu.record_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/krupnoveo/edu/record_service/RecordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krupnoveo.edu.record_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
